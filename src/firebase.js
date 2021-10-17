4f1cc725d7383ec974dded1a74bfc8f109d794a0
Carlos Gonzalez <carlosigonz@gmail.com>
2021-10-17 16:21:00 -0400

    Arreglado Firebase Imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const config = {
     apiKey: "AIzaSyBlgB-t0QQl5APvJYuvAujeqJMiQfOJK_A",
