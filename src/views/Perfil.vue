5171a5b0f8704b8ebf9ef4f76c34340ced095809
Ricardo Micale <ricardomicale@gmail.com>
2021-10-20 18:08:49 +0200

    perfil mvp css terminado

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>{{ user.nombre }}</div>
-  <p>{{ user.creditosTot }}</p>
+  <div class="contenido">
+    <div class="info-estudiante">
+      <section class="info-contacto">
+        <h2>{{ user.nombre }}</h2>
+        <h4>{{ user.carrera }}</h4>
+        <p>
+          Contacto: <a :href="'mailto:' + user.email">{{ user.email }}</a>
+        </p>
+      </section>
+      <section class="info-carrera">
+        <p>
+          Cantidad total de creditos acumulados:
+          <span>{{ user.creditosTot }}</span>
+        </p>
+        <p>
+          Cantidad de creditos en materias BP:
+          <span>{{ user.creditosBP }}</span>
+        </p>
+        <p>
+          Cantiadad de creditos faltantes pir cursar:
+          <span>
+            {{ user.creditosFaltantes }}
+          </span>
+        </p>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -31,7 +56,6 @@ export default {
         this.$router.push("/");
       }
     });
-    console.log(this.user);
     return {
       user,
     };
@@ -40,4 +64,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../variabes.scss";
+
+.contenido {
+  padding: 1.5rem 3rem;
+
+  .info-estudiante {
+    color: $font;
+
+    a {
+      color: $font;
+      text-decoration: none;
+    }
+
+    .info-contacto {
+      margin: 2rem 2rem 1rem;
+
+      h2 {
+        margin-bottom: 0.6rem;
+      }
+
+      h4,
+      p {
+        opacity: 0.8;
+        margin-bottom: 0.2rem;
+      }
+    }
+
+    .info-carrera {
+      margin: 2rem 2rem 1rem;
+
+      p {
+        margin: 0.3rem 0;
+
+        span {
+          font-weight: bold;
+        }
+      }
+    }
+  }
+}
 </style>
