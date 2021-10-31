6f32397f2cac5b0128dd3f84c637a109647e3150
Ricardo Micale <ricardomicale@gmail.com>
2021-10-31 23:38:28 +0100

    bug del comentario arreglado

--- src/components/Comentario.vue
+++ src/components/Comentario.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>{{ comment.nombre }}</div>
-  <div>{{ comment.comentario }}</div>
+  <div v-if="comment">
+    <div>{{ comment.nombre }}</div>
+    <div>{{ comment.comentario }}</div>
+  </div>
+  <div v-else>
+    Aun no se ha empezado la discusion! Se el primero en comentar esta materia
+  </div>
 </template>
 
 <script>
