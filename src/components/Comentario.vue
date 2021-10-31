ef629d2cc3ce8a756d2ef0a074ae706f1d69b396
Ricardo Micale <ricardomicale@gmail.com>
2021-10-31 23:32:08 +0100

    creacion de la discusion

---
+++ src/components/Comentario.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ comment.nombre }}</div>
+  <div>{{ comment.comentario }}</div>
+</template>
+
+<script>
+export default {
+  name: "Comentario",
+  props: {
+    comment: Object,
+  },
+};
+</script>
+
+<style>
+</style>
