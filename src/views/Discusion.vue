a4e5e6deb5da9b10e2c21b4f092b70d18511e015
Ricardo Micale <ricardomicale@gmail.com>
2021-11-05 18:46:23 +0100

    vista de discusion empezada

--- src/views/Discusion.vue
+++ src/views/Discusion.vue
@@ -1,11 +1,15 @@
 <template>
-
-   <h1>Discusion</h1>
-    <h3>Agregar comentario +</h3> 
-<div class="cont">
+  <div class="comment-section">
+    <h1>Discusion</h1>
+    <button class="add-comment" @click="aggComment()">
+      Agregar comentario<font-awesome-icon
+        icon="plus"
+        class="fas"
+      ></font-awesome-icon>
+    </button>
     <section class="comentarios">
       <div class="comentario" v-for="(comment, index) in comments" :key="index">
-        <Comentario  :comment="comment" class="comenta"/>
+        <Comentario :comment="comment" class="comenta" />
       </div>
     </section>
   </div>
@@ -25,6 +29,7 @@ export default {
     return {
       comments: [],
       materia: {},
+      agregar: false,
     };
   },
   created() {
@@ -48,46 +53,45 @@ export default {
       }
     });
   },
+  methods: {
+    aggComment() {
+      this.false = !this.false;
+    },
+  },
 };
 </script>-->
 
 <style lang="scss" scoped>
 @import "../variabes.scss";
-  h1{
-    color: $font;
-    padding: 2%;
+
+.comment-section {
+  padding: 0 3rem;
+}
+
+h1 {
+  color: $font;
+  margin-top: 2rem;
+}
+
+.add-comment {
+  padding: 0.6rem 1.4rem;
+  background-color: $acento;
+  font-family: $fonts;
+  outline: none;
+  border: none;
+  color: $font;
+  margin: 1rem 0;
+  border-radius: 0.4rem;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  .fas {
+    margin-left: 0.5rem;
   }
-  
-  .cont{
-  width: 80%;
-  height:  80vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: auto;
 
-}
-.comentarios{
-  width: 50%;
-  height: 100%;
-  
-}
-.comentario{
-  width: 100%;
-  height: 30vh;
-  margin-top: 2%;
-}
-.comenta{
-  width: 100%;
-  height: 100%;
-  background-color: $bg-secundario;
-}
-
-h3{
-  color: white;
-  margin-right: 0;
-  padding-right: 2%;
-  padding-left: 2%;
-  text-align: right;
+  &:active {
+    background-color: $button-press;
+    color: $input-bg-alt;
+  }
 }
 </style>
