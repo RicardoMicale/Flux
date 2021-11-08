fbfd754ba0d4c1b1797138ec943649306050c591
Ricardo Micale <ricardomicale@gmail.com>
2021-11-08 15:28:22 +0100

    link a redes desde el comentario

--- src/main.js
+++ src/main.js
@@ -13,20 +13,27 @@ import {
     faTimes,
     faEnvelope
 } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {
+    faTwitter,
+    faInstagram,
+  } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faAngleRight);
-library.add(faEye);
-library.add(faCheck);
-library.add(faCircle);
-library.add(faAngleLeft);
-library.add(faExternalLinkAlt);
-library.add(faPlus);
-library.add(faTimes);
-library.add(faEnvelope);
-library.add(faTwitter);
-library.add(faInstagram);
+library.add(
+    faAngleRight, faEye, faCheck, faCircle, faAngleLeft, 
+    faExternalLinkAlt, faPlus, faTimes, faEnvelope, 
+    faTwitter, faInstagram
+    );
+// library.add(faEye);
+// library.add(faCheck);
+// library.add(faCircle);
+// library.add(faAngleLeft);
+// library.add(faExternalLinkAlt);
+// library.add(faPlus);
+// library.add(faTimes);
+// library.add(faEnvelope);
+// library.add(faTwitter);
+// library.add(faInstagram);
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
